682fa63f374c4255d8363c095adbf9504e8b4902
jeyj0 <jeyj0.plus@gmail.com>
2018-01-30 11:08:19 +0100

    Added profile links

--- src/ui/components/Footer/Footer.js
+++ src/ui/components/Footer/Footer.js
@@ -1,14 +1,44 @@
 import "./style.scss";
 import { Component } from "preact";
-import Link from "preact-router";
+import { Link } from "preact-router/match";
 
 export default class Footer {
   render() {
     return (
       <div class="footer">
-        <span>Jannis Jorre</span>
-        <span>jeyj0.plus@gmail.com</span>
-        {/*<Link href="/impressum">Impressum</Link>*/}
+        <address>
+          <span>Jannis Jorre</span>
+          <ul>
+            <li>
+              <span>
+                <a href="mailto:jeyj0.plus@gmail.com">jeyj0.plus@gmail.com</a>
+              </span>
+            </li>
+            <li>
+              <span>
+                <a
+                  href="https://github.com/jeyj0"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  GitHub
+                </a>
+              </span>
+            </li>
+            <li>
+              <span>
+                <a
+                  href="https://www.xing.com/profile/Jannis_Jorre/"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  Xing
+                </a>
+              </span>
+            </li>
+          </ul>
+        </address>
+        <Link href="/impressum">Impressum</Link>
       </div>
     );
   }
